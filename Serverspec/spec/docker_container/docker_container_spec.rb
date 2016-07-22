2beb001d138009212a14b5536f68e1cd4f0191f0
Tatsunori Saito <1.boogie.5.sophia.4.pop.0@gmail.com>
2016-07-22 23:38:00 +0900

    serverspec mod

--- Serverspec/spec/docker_container/docker_container_spec.rb
+++ Serverspec/spec/docker_container/docker_container_spec.rb
@@ -3,9 +3,27 @@ require 'docker_container/spec_helper.rb'
 describe ("docker_container") do
   tls = property['docker_container']['tls']['enable'] ? "DOCKER_TLS_VERIFY=1" : ""
   property['docker_container']['containers'].each do |container|
-    describe ("check container is running") do
-      describe command("DOCKER_HOST=#{property['docker_container']['docker_host']} #{tls} docker inspect #{container['name']}") do
-        its(:exit_status) { should eq 0 }
+    if container['state'] == "started"
+      describe ("check container is running") do
+        describe command("DOCKER_HOST=#{property['docker_container']['docker_host']} #{tls} docker inspect #{container['name']}") do
+          its(:exit_status) { should eq 0 }
+        end
+      end
+    end
+
+    if container['state'] == "stopped"
+      describe ("check container is stopped") do
+        describe command("DOCKER_HOST=#{property['docker_container']['docker_host']} #{tls} docker inspect #{container['name']} | grep -e \"Running\" | grep -e \"false\"") do
+          its(:exit_status) { should eq 0 }
+        end
+      end
+    end
+
+    if container['state'] == "absent"
+      describe ("check container is absent") do
+        describe command("DOCKER_HOST=#{property['docker_container']['docker_host']} #{tls} docker inspect #{container['name']}") do
+          its(:exit_status) { should_not eq 0 }
+        end
       end
     end
   end

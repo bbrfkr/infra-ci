31e1e67688adee629f38367a05cda607c0988098
Tatsunori Saito <1.boogie.5.sophia.4.pop.0@gmail.com>
2016-07-22 23:40:16 +0900

    kicker

--- Kicker/docker_container.sh
+++ Kicker/docker_container.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 export PATH=${PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:
 
-### Ansible(pre)
-cd Ansible
-ansible-art -V
+### Vagrant(pre)
+cd Vagrant/docker_container
+vagrant up
+
+### Ansible
+cd ../../Ansible
 ROLES_DIR=`pwd`/roles
 sed -i -e "s|roles_path\s*=\s*.\+|roles_path = ${ROLES_DIR}|g" ~/.ansible-art.cfg
-ansible-art apply docker_container inventories/hosts_docker_container -p host_vars_dir/docker_container_create -a "-s"
+ansible-art apply vagrant_root inventories/vagrant_root -a "-s"
+ansible-art apply docker inventories/docker
+ansible-art apply docker_container inventories/docker_container
 
 ### Serverspec
 cd ../Serverspec
 rake -f Rakefiles/docker_container spec
 RC=$?
 
-### Ansible(post)
-cd ../Ansible
-ansible-art apply docker_container inventories/hosts_docker_container -p host_vars_dir/docker_container_delete -a "-s"
+### Vagrant(post)
+cd ../Vagrant/docker_container
+vagrant destroy -f
 
 exit $RC

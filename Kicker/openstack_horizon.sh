1ab7908d15f2f5c25d3866e20e8489ef0d886c38
Tatsunori Saito <1.boogie.5.sophia.4.pop.0@gmail.com>
2016-11-01 22:38:53 +0900

    ansible

---
+++ Kicker/openstack_horizon.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+export PATH=${PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:
+
+### Vagrant(pre)
+cd Vagrant/openstack_horizon
+vagrant up
+
+### Ansible(pre)
+cd ../../Ansible
+ROLES_DIR=`pwd`/roles
+sed -i -e "s|roles_path\s*=\s*.\+|roles_path = ${ROLES_DIR}|g" ~/.ansible-art.cfg
+ansible-art apply vagrant_root inventories/vagrant_root -a "-s"
+ansible-art apply openstack_packages inventories/openstack_packages
+ansible-art apply openstack_mariadb inventories/openstack_mariadb
+ansible-art apply openstack_memcached inventories/openstack_memcached
+ansible-art apply ansible_keyfiles inventories/ansible_keyfiles
+ansible-art apply openstack_keystone_install inventories/openstack_keystone_install -p host_vars_dir/openstack_keystone_install_01
+ansible-art apply openstack_keystone_setup inventories/openstack_keystone_setup -p host_vars_dir/openstack_keystone_setup_01
+ansible-art apply openstack_nova_controller inventories/openstack_nova_controller -p host_vars_dir/openstack_nova_controller_01
+ansible-art apply openstack_neutron_controller inventories/openstack_neutron_controller -p host_vars_dir/openstack_neutron_controller_01
+ansible-art apply openstack_horizon inventories/openstack_horizon -p host_vars_dir/openstack_horizon_01
+
+### Serverspec
+#cd ../Serverspec
+#rake -f Rakefiles/openstack_horizon spec
+#RC=$?
+
+### Vagrant(post)
+cd ../Vagrant/openstack_horizon
+vagrant destroy -f
+
+exit $RC
